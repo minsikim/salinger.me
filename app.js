533813ad6e641d8e35b9172b92eb3579585e609b
minsikim <minsiua@gmail.com>
2018-02-13 13:33:33 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// This includes:
+// express.js
+// view-engine.js
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+// app.use(express.static(__dirname + '/public'));
+
+
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+app.listen(3000, function(){
+  console.log('connected 3000 port!');
+});
+
+app.get('/', (req,res) => {
+  res.render('index');
+});
